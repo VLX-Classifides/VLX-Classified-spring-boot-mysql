f64586210c3bf3ff19d6262f5d911099f8c600fc
Pratik Mukherjee <pratikmukherjee32@gmail.com>
2021-09-15 21:25:53 +0530

    command line runner used for pre-creating users

--- src/main/java/com/webapp/demo/VlxJwtMySqlApplication.java
+++ src/main/java/com/webapp/demo/VlxJwtMySqlApplication.java
@@ -1,13 +1,33 @@
 package com.webapp.demo;
 
+import com.webapp.demo.model.User;
+import com.webapp.demo.repo.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class VlxJwtMySqlApplication {
+public class VlxJwtMySqlApplication implements CommandLineRunner {
+
+	@Autowired
+	UserRepo userRepo;
 
 	public static void main(String[] args) {
 		SpringApplication.run(VlxJwtMySqlApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception{
+		User user = userRepo.findByEmail("admin@gmail.com");
+		if(user.getId() == 0){
+			User user1 = new User("admin@gmail.com", "admin", "admin");
+			User user2 = new User("buyer@gmail.com", "buyer", "buyer");
+			User user3 = new User("seller@gmail.com", "seller", "seller");
+			userRepo.save(user1);
+			userRepo.save(user2);
+			userRepo.save(user3);
+		}
 	}
 
 }
