09609d1156e4bfce504d429686ed0846dd9dc15e
Pratik Mukherjee <pratikmukherjee32@gmail.com>
2021-09-15 21:25:24 +0530

    custom functions added for products repo

--- src/main/java/com/webapp/demo/repo/ProductsRepo.java
+++ src/main/java/com/webapp/demo/repo/ProductsRepo.java
@@ -6,6 +6,6 @@ import org.springframework.stereotype.Repository;
 import com.webapp.demo.model.Products;
 
 @Repository
-public interface ProductsRepo extends JpaRepository<Products, Integer>{
+public interface ProductsRepo extends JpaRepository<Products, Integer>, ProductRepoCustom{
 	
 }
