9784ad1df7663c6ec7f69af9ea472e5d94a6ec8b
Pratik Mukherjee <pratikmukherjee32@gmail.com>
2021-09-26 21:17:51 +0530

    service bug

--- src/main/java/com/webapp/demo/service/imageService.java
+++ src/main/java/com/webapp/demo/service/imageService.java
@@ -10,7 +10,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 
 @Service
-public class ImageService {
+public class imageService {
 
     @Autowired
     ProductsRepo productsRepo;
